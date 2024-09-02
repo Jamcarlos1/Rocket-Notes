4ea2bd30e1cce1eb8fa87b3979843af5e7c2788d
jean <jeancarlossilva1234@gmail.com>
2024-09-02 20:21:58 -0300

    commit pasta rocketnotes

---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { container } from './styles';
+
+export function details(){
+
+  return(
+    <container>
+      <h1>Hello World!</h1>
+      <span>Jean carlos</span>
+    </container>
+     
+  )
+}
